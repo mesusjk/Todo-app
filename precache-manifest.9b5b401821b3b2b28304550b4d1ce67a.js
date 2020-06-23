344772e69ac0af7d0ade378d81d2f2bd9ac69573
mesusjk <64482133+mesusjk@users.noreply.github.com>
2020-06-23 13:54:48 +0430

    Updates

---
+++ precache-manifest.9b5b401821b3b2b28304550b4d1ce67a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "961488600d2f27b6a9197c6e359863e9",
+    "url": "/Todo-app/index.html"
+  },
+  {
+    "revision": "5a2189dd5efd4dc05435",
+    "url": "/Todo-app/static/css/main.deca8e79.chunk.css"
+  },
+  {
+    "revision": "6f1be88981171502cb75",
+    "url": "/Todo-app/static/js/2.227e20d3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Todo-app/static/js/2.227e20d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a2189dd5efd4dc05435",
+    "url": "/Todo-app/static/js/main.822c8c69.chunk.js"
+  },
+  {
+    "revision": "abfe733872dcab3b4805",
+    "url": "/Todo-app/static/js/runtime-main.3de85337.js"
+  }
+]);
